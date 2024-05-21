27a53520f97bf11ae2ba36ea298d28fa953af07b
Renan Machado <renanoliveira2199@gmail.com>
2024-05-20 22:41:06 -0300

    Working with four verbs

---
+++ 02_rwaspnet5_Calculator/rwaspnet5/rwaspnet5/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace rwaspnet5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Invalid Input");
+        }
+
+    }
+}
