96b1eb0a73c4f0a7c88623a9fa6a4b4d79f226b1
Renan Machado <renanoliveira2199@gmail.com>
2024-05-20 23:25:12 -0300

    Implementing Person interface and services implementations

---
+++ 02_rwaspnet5_Calculator/rwaspnet5/rwaspnet5/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using rwaspnet5.Model;
+
+namespace rwaspnet5.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
